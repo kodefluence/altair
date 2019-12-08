b5f9df2adf1823fe7bc9c41b68e0a29e05f4b2e3
insomnius <aliana.arib@gmail.com>
2019-12-09 00:20:09 +0700

    Add gitignore and fabricate connection

--- altair.go
+++ altair.go
@@ -1,7 +1,22 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/codefluence-x/journal"
 	"github.com/spf13/cobra"
+)
+
+var (
+	mysqlDB              *sql.DB
+	mysqlConnMaxLifetime time.Duration
+	mysqlMaxIdleConn     int
+	mysqlMaxOpenConn     int
 )
 
 func main() {
@@ -17,5 +32,54 @@ func executeCommand() {
 		},
 	}
 
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "Run API gateway services.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fabricateConnection()
+		},
+	}
+
+	rootCmd.AddCommand(runCmd)
 	_ = rootCmd.Execute()
+}
+
+func fabricateConnection() error {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&interpolateParams=true", os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_NAME")))
+	if err != nil {
+		journal.Error(fmt.Sprintln("Fabricate connection error:", err), err).SetTags("altair", "main").Log()
+		return err
+	}
+	db.SetConnMaxLifetime(mysqlConnMaxLifetime)
+	db.SetMaxIdleConns(mysqlMaxIdleConn)
+	db.SetMaxOpenConns(mysqlMaxOpenConn)
+
+	mysqlDB = db
+
+	journal.Info(fmt.Sprintf("Complete fabricating mysql writer connection: %s:%s@tcp(%s)/%s?", os.Getenv("DATABASE_USERNAME"), "***********", os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_NAME"))).SetTags("altair", "main").Log()
+
+	return nil
+}
+
+func closeConnection() {
+	if mysqlDB != nil {
+		var err error
+		for i := 0; i < 3; i++ {
+			err = mysqlDB.Close()
+			if err != nil {
+				journal.Error(fmt.Sprintln("Error closing mysql writer because of:", err), err).SetTags("altair", "main").Log()
+				continue
+			}
+
+			if err == nil {
+				break
+			}
+		}
+		if err != nil {
+			journal.Info("Failed closing mysql writer and reader connection.").SetTags("altair", "main").Log()
+			return
+		}
+
+		journal.Info("Success closing mysql writer and reader connection.").SetTags("altair", "main").Log()
+	}
 }
