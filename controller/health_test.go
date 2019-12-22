2e718c6d9120c51e580128118b0c3f10a4ab3b71
insomnius <aliana.arib@gmail.com>
2019-12-22 15:20:22 +0700

    Add unit test for health

---
+++ controller/health_test.go
@@ -0,0 +1,23 @@
+package controller_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/codefluence-x/altair/controller"
+	"github.com/codefluence-x/altair/mock"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealth(t *testing.T) {
+	t.Run("Health", func(t *testing.T) {
+		t.Run("Return OK response", func(t *testing.T) {
+			gin.SetMode(gin.ReleaseMode)
+			engine := gin.New()
+			engine.GET("/health", controller.Health)
+			w := mock.PerformRequest(engine, "GET", "/health", nil)
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+	})
+}
