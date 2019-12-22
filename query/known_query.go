af9e78039080e6365a0fe55ec17223c287059619
insomnius <aliana.arib@gmail.com>
2019-12-22 14:13:01 +0700

    Add as total

--- query/known_query.go
+++ query/known_query.go
@@ -1,6 +1,6 @@
 package query
 
 var PaginateOauthApplication = "select * from oauth_applications limit ?, ?"
-var CountOauthApplication = "select count(*) from oauth_applications where revoked_at is null"
+var CountOauthApplication = "select count(*) as total from oauth_applications where revoked_at is null"
 var SelectOneOauthApplication = "select * from oauth_applications where id = ?"
 var InsertOauthApplication = "insert into oauth_applications (owner_id, description, scopes, client_uid, client_secret, revoked_at, created_at, updated_at) values(?, ?, ?, ?, ?, null, now(), now())"
