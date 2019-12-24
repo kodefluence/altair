13b9ecaa647348accca0914e74ca41b5cfa42364
insomnius <aliana.arib@gmail.com>
2019-12-25 06:26:07 +0700

    Change array of entity errors into array of entity object

--- mock/test_helper.go
+++ mock/test_helper.go
@@ -4,7 +4,13 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+
+	entity "github.com/codefluence-x/altair/entity"
 )
+
+type ErrorResponse struct {
+	Errors []entity.ErrorObject `json:"errors"`
+}
 
 func PerformRequest(r http.Handler, method, path string, body io.Reader) *httptest.ResponseRecorder {
 	req, _ := http.NewRequest(method, path, body)
