b18f64cc5adc3c78b9e1b7e47f32eb24b7de0e36
insomnius <aliana.arib@gmail.com>
2020-01-05 12:58:11 +0700

    Generate new mock for model

--- mock/mock_model.go
+++ mock/mock_model.go
@@ -6,6 +6,7 @@ package mock
 
 import (
 	context "context"
+	sql "database/sql"
 	entity "github.com/codefluence-x/altair/entity"
 	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
@@ -46,6 +47,49 @@ func (m *MockHasName) Name() string {
 func (mr *MockHasNameMockRecorder) Name() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Name", reflect.TypeOf((*MockHasName)(nil).Name))
+}
+
+// MockDBExecutable is a mock of DBExecutable interface
+type MockDBExecutable struct {
+	ctrl     *gomock.Controller
+	recorder *MockDBExecutableMockRecorder
+}
+
+// MockDBExecutableMockRecorder is the mock recorder for MockDBExecutable
+type MockDBExecutableMockRecorder struct {
+	mock *MockDBExecutable
+}
+
+// NewMockDBExecutable creates a new mock instance
+func NewMockDBExecutable(ctrl *gomock.Controller) *MockDBExecutable {
+	mock := &MockDBExecutable{ctrl: ctrl}
+	mock.recorder = &MockDBExecutableMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use
+func (m *MockDBExecutable) EXPECT() *MockDBExecutableMockRecorder {
+	return m.recorder
+}
+
+// Exec mocks base method
+func (m *MockDBExecutable) Exec(query string, args ...interface{}) (sql.Result, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{query}
+	for _, a := range args {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "Exec", varargs...)
+	ret0, _ := ret[0].(sql.Result)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Exec indicates an expected call of Exec
+func (mr *MockDBExecutableMockRecorder) Exec(query interface{}, args ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{query}, args...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Exec", reflect.TypeOf((*MockDBExecutable)(nil).Exec), varargs...)
 }
 
 // MockOauthApplicationModel is a mock of OauthApplicationModel interface
@@ -113,4 +157,24 @@ func (m *MockOauthApplicationModel) Count(ctx context.Context) (int, error) {
 func (mr *MockOauthApplicationModelMockRecorder) Count(ctx interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Count", reflect.TypeOf((*MockOauthApplicationModel)(nil).Count), ctx)
+}
+
+// Create mocks base method
+func (m *MockOauthApplicationModel) Create(ctx context.Context, data *entity.OauthApplicationJSON, txs ...*sql.Tx) (int, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{ctx, data}
+	for _, a := range txs {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "Create", varargs...)
+	ret0, _ := ret[0].(int)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Create indicates an expected call of Create
+func (mr *MockOauthApplicationModelMockRecorder) Create(ctx, data interface{}, txs ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{ctx, data}, txs...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Create", reflect.TypeOf((*MockOauthApplicationModel)(nil).Create), varargs...)
 }
