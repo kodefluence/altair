41302561fb7a0dd53adf8e9beae6cbf234d7cf65
insomnius <aliana.arib@gmail.com>
2020-01-12 23:42:09 +0700

    Create entity for authorization and access token request

--- entity/entity.go
+++ entity/entity.go
@@ -32,3 +32,23 @@ type OauthApplicationJSON struct {
 	CreatedAt    *time.Time `json:"created_at"`
 	UpdatedAt    *time.Time `json:"updated_at"`
 }
+
+type AuthorizationRequestJSON struct {
+	ResponseType *string `json:"response_type"`
+
+	ClientUID    *string `json:"client_uid"`
+	ClientSecret *string `json:"client_secret"`
+
+	RedirectURI *string `json:"redirect_uri"`
+	Scopes      *string `json:"scopes"`
+}
+
+type AccessTokenRequestJSON struct {
+	GrantType *string `json:"grant_type"`
+
+	ClientUID    *string `json:"client_uid"`
+	ClientSecret *string `json:"client_secret"`
+
+	Code        *string `json:"code"`
+	RedirectURI *string `json:"redirect_uri"`
+}
