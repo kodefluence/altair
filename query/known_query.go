4a39b11b2a405df59341d6044dfd18add94b1d6e
insomnius <aliana.arib@gmail.com>
2020-01-13 00:18:24 +0700

    Create new query to select oauth application by its client_uid and client_secret

--- query/known_query.go
+++ query/known_query.go
@@ -4,6 +4,7 @@ package query
 var PaginateOauthApplication = "select * from oauth_applications limit ?, ?"
 var CountOauthApplication = "select count(*) as total from oauth_applications where revoked_at is null"
 var SelectOneOauthApplication = "select * from oauth_applications where id = ?"
+var SelectOneByUIDandSecret = "select * from oauth_applications where client_uid = ? and client_secret = ? limit 1"
 var InsertOauthApplication = "insert into oauth_applications (owner_id, owner_type, description, scopes, client_uid, client_secret, revoked_at, created_at, updated_at) values(?, ?, ?, ?, ?, ?, null, now(), now())"
 
 // oauth_access_tokens
--- query/known_query_test.go
+++ query/known_query_test.go
@@ -11,6 +11,7 @@ func TestQuery(t *testing.T) {
 	assert.Equal(t, "select * from oauth_applications limit ?, ?", query.PaginateOauthApplication)
 	assert.Equal(t, "select count(*) as total from oauth_applications where revoked_at is null", query.CountOauthApplication)
 	assert.Equal(t, "select * from oauth_applications where id = ?", query.SelectOneOauthApplication)
+	assert.Equal(t, "select * from oauth_applications where client_uid = ? and client_secret = ? limit 1", query.SelectOneByUIDandSecret)
 	assert.Equal(t, "insert into oauth_applications (owner_id, owner_type, description, scopes, client_uid, client_secret, revoked_at, created_at, updated_at) values(?, ?, ?, ?, ?, ?, null, now(), now())", query.InsertOauthApplication)
 
 	assert.Equal(t, "insert into oauth_access_tokens (oauth_application_id, resource_owner_id, token, scopes, expires_in, created_at, revoked_at) values(?, ?, ?, ?, ?, now(), null)", query.InsertOauthAccessToken)
