68a7642676644f901d1ed3dd063adccdc1d75ac4
insomnius <aliana.arib@gmail.com>
2020-01-12 23:29:03 +0700

    Create migration for oauth access grants table

--- migration/3_create_table_oauth_access_grants.up.sql
+++ migration/3_create_table_oauth_access_grants.up.sql
@@ -1,17 +1,17 @@
-CREATE TABLE `oauth_access_tokens` (
+CREATE TABLE `oauth_access_grants` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 
   `oauth_application_id` int(11) unsigned NOT NULL,
   `resource_owner_id` int(11) unsigned NOT NULL,
 
-  `token` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
 
   `expires_in` DATETIME NOT NULL,
   `created_at` DATETIME NOT NULL,
   `revoked_at` DATETIME DEFAULT NULL,
 
   PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`),
+  UNIQUE KEY `code` (`code`),
   KEY `id_oauth_application_id` (`id`, `oauth_application_id`),
   KEY `id_oauth_application_id_resource_owner_id` (`id`, `oauth_application_id`, `resource_owner_id`),
   KEY `oauth_application_id_resource_owner_id` (`oauth_application_id`, `resource_owner_id`)
