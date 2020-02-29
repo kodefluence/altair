0cf3a8f35d9a4fb8d2bb3800a161e9276ae24374
insomnius <aliana.arib@gmail.com>
2020-02-29 08:13:16 +0700

    Completing grants controller

--- controller/oauth/authorization/grant.go
+++ controller/oauth/authorization/grant.go
@@ -1,9 +1,13 @@
 package authorization
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/codefluence-x/altair/core"
+	"github.com/codefluence-x/altair/entity"
+	"github.com/codefluence-x/altair/eobject"
+	"github.com/codefluence-x/journal"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,6 +30,41 @@ func (o *grantController) Path() string {
 }
 
 func (o *grantController) Control(c *gin.Context) {
+	var req entity.AuthorizationRequestJSON
 
-	c.JSON(http.StatusOK, gin.H{})
+	rawData, err := c.GetRawData()
+	if err != nil {
+		journal.Error("Cannot get raw data", err).
+			SetTags("controller", "authorization", "grant", "get_raw_data").
+			Log()
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": eobject.Wrap(eobject.BadRequestError("request body")),
+		})
+		return
+	}
+
+	err = json.Unmarshal(rawData, &req)
+	if err != nil {
+		journal.Error("Cannot unmarshal json", err).
+			SetTags("controller", "authorization", "grant", "unmarshal").
+			Log()
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": eobject.Wrap(eobject.BadRequestError("request body")),
+		})
+		return
+	}
+
+	data, entityErr := o.authService.Grantor(c, req)
+	if entityErr != nil {
+		c.JSON(entityErr.HttpStatus, gin.H{
+			"errors": entityErr.Errors,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"data": data,
+	})
 }
