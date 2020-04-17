8fa13f08af3d718dd80c0ef126fb3c29381d3895
insomnius <aliana.arib@gmail.com>
2020-04-18 00:57:44 +0700

    Create route object entity

--- entity/entity.go
+++ entity/entity.go
@@ -127,3 +127,12 @@ type OauthAccessGrantInsertable struct {
 	RedirectURI        interface{}
 	ExpiresIn          time.Time
 }
+
+type RouteObject struct {
+	Name   string `yaml:"name"`
+	Auth   string `yaml:"auth"`
+	Prefix string `yaml:"prefix"`
+	Host   string `yaml:"host"`
+	Path   map[string]struct {
+	} `yaml:"path"`
+}
