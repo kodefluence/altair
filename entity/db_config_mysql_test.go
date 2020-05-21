0f0cf30586dab9c798bb02147a55a51628a644f0
insomnius <aliana.arib@gmail.com>
2020-05-22 01:38:12 +0700

    Add more coverage for entity package

--- entity/db_config_mysql_test.go
+++ entity/db_config_mysql_test.go
@@ -1,7 +1,53 @@
 package entity_test
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/codefluence-x/altair/entity"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMYSQLDatabaseConfig(t *testing.T) {
+	MYSQLConfig := entity.MYSQLDatabaseConfig{
+		Database:              "altair_development",
+		Username:              "some_username",
+		Password:              "some_password",
+		Host:                  "localhost",
+		Port:                  "3306",
+		ConnectionMaxLifetime: "120s",
+		MaxIddleConnection:    "100",
+		MaxOpenConnection:     "100",
+	}
 
+	expectedDatabase := "altair_development"
+	expectedUsername := "some_username"
+	expectedPassword := "some_password"
+	expectedHost := "localhost"
+	expectedPort := 3306
+	expectedConnMaxLifetime := time.Second * 120
+	expectedMaxIddleConn := 100
+	expectedMaxOpenConn := 100
+
+	assert.Equal(t, "mysql", MYSQLConfig.Driver())
+	assert.Equal(t, expectedDatabase, MYSQLConfig.DBDatabase())
+	assert.Equal(t, expectedUsername, MYSQLConfig.DBUsername())
+	assert.Equal(t, expectedPassword, MYSQLConfig.DBPassword())
+	assert.Equal(t, expectedHost, MYSQLConfig.DBHost())
+
+	actualPort, err := MYSQLConfig.DBPort()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedPort, actualPort)
+
+	actualConnMaxLifetime, err := MYSQLConfig.DBConnectionMaxLifetime()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedConnMaxLifetime, actualConnMaxLifetime)
+
+	actualMaxIddleConn, err := MYSQLConfig.DBMaxIddleConn()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedMaxIddleConn, actualMaxIddleConn)
+
+	actualMaxOpenConn, err := MYSQLConfig.DBMaxOpenConn()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedMaxOpenConn, actualMaxOpenConn)
 }
