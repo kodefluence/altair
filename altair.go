6e71dc6ce24ffa615b9361f64282884ebab3b5c2
insomnius <aliana.arib@gmail.com>
2020-06-08 23:19:06 +0700

    Create migration provider command

--- altair.go
+++ altair.go
@@ -203,7 +203,7 @@ func executeCommand() {
 
 	migrateDownCmd := &cobra.Command{
 		Use:   "migrate:down",
-		Short: "Do a migration from current version into latest versions.",
+		Short: "Down the migration from current version into earlies versions.",
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) < 1 {
 				fmt.Println("Invalid number of arguments, expected 1. Example `altair migrate:down [database_instance_name]`.")
@@ -240,7 +240,46 @@ func executeCommand() {
 		},
 	}
 
-	rootCmd.AddCommand(runCmd, migrateCmd, migrateDownCmd, configCmd)
+	migrateRollbackCmd := &cobra.Command{
+		Use:   "migrate:rollback",
+		Short: "Do a migration rollback from current versions into previous versions.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Println("Invalid number of arguments, expected 1. Example `altair migrate:rollback [database_instance_name]`.")
+				fmt.Println("To see available database instance, use `altair config db`.")
+				return
+			}
+
+			defer closeConnection()
+			if err := fabricateConnection(); err != nil {
+				return
+			}
+
+			dbBearer := loader.DatabaseBearer(databases, dbConfigs)
+			db, config, err := dbBearer.Database(args[0])
+			if err != nil {
+				journal.Error(fmt.Sprintf("Error loading database instance of: `%s`", args[0]), err).SetTags("altair", "main").Log()
+				return
+			}
+
+			migrationProvider := provider.Migration().GoMigrate(db, config)
+			migrator, err := migrationProvider.Migrator()
+			if err != nil {
+				journal.Error("Error providing migrator", err).SetTags("altair", "main").Log()
+				return
+			}
+			defer migrator.Close()
+
+			if err := migrator.Steps(-1); err != nil && err.Error() != "no change" {
+				journal.Error("Error doing database migration", err).SetTags("altair", "main").Log()
+				return
+			}
+
+			journal.Info("Successfully migrating databases").SetTags("altair", "main").Log()
+		},
+	}
+
+	rootCmd.AddCommand(runCmd, migrateCmd, migrateDownCmd, migrateRollbackCmd, configCmd)
 	_ = rootCmd.Execute()
 }
 
