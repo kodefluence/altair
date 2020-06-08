ecce089513382acc9f8f62b852a7db6f93bc14b1
insomnius <aliana.arib@gmail.com>
2020-06-08 23:15:57 +0700

    Create migrate down command

--- altair.go
+++ altair.go
@@ -190,6 +190,7 @@ func executeCommand() {
 				journal.Error("Error providing migrator", err).SetTags("altair", "main").Log()
 				return
 			}
+			defer migrator.Close()
 
 			if err := migrator.Up(); err != nil && err.Error() != "no change" {
 				journal.Error("Error doing database migration", err).SetTags("altair", "main").Log()
@@ -200,7 +201,46 @@ func executeCommand() {
 		},
 	}
 
-	rootCmd.AddCommand(runCmd, migrateCmd, configCmd)
+	migrateDownCmd := &cobra.Command{
+		Use:   "migrate:down",
+		Short: "Do a migration from current version into latest versions.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Println("Invalid number of arguments, expected 1. Example `altair migrate:down [database_instance_name]`.")
+				fmt.Println("To see available database instance, use `altair config db`.")
+				return
+			}
+
+			defer closeConnection()
+			if err := fabricateConnection(); err != nil {
+				return
+			}
+
+			dbBearer := loader.DatabaseBearer(databases, dbConfigs)
+			db, config, err := dbBearer.Database(args[0])
+			if err != nil {
+				journal.Error(fmt.Sprintf("Error loading database instance of: `%s`", args[0]), err).SetTags("altair", "main").Log()
+				return
+			}
+
+			migrationProvider := provider.Migration().GoMigrate(db, config)
+			migrator, err := migrationProvider.Migrator()
+			if err != nil {
+				journal.Error("Error providing migrator", err).SetTags("altair", "main").Log()
+				return
+			}
+			defer migrator.Close()
+
+			if err := migrator.Down(); err != nil && err.Error() != "no change" {
+				journal.Error("Error doing database migration", err).SetTags("altair", "main").Log()
+				return
+			}
+
+			journal.Info("Successfully migrating databases").SetTags("altair", "main").Log()
+		},
+	}
+
+	rootCmd.AddCommand(runCmd, migrateCmd, migrateDownCmd, configCmd)
 	_ = rootCmd.Execute()
 }
 
