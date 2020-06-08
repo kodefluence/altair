b48a941bf18caa6d124584f0587ac5fd689f7ae5
insomnius <aliana.arib@gmail.com>
2020-06-09 01:06:56 +0700

    Create method to parse duration

--- entity/plugin_config_oauth.go
+++ entity/plugin_config_oauth.go
@@ -2,14 +2,23 @@ package entity
 
 import "time"
 
-type OauthPlugins struct {
+type OauthPlugin struct {
 	Config struct {
 		Database string `yaml:"database"`
 
-		AccessTokenTimeoutRaw string        `yaml:"access_token_timeout"`
-		AccessTokenTimeout    time.Duration `yaml:"-"`
-
-		AuthorizationCodeTimeoutRaw string        `yaml:"authorization_code_timeout"`
-		AuthorizationCodeTimeout    time.Duration `yaml:"-"`
+		AccessTokenTimeoutRaw       string `yaml:"access_token_timeout"`
+		AuthorizationCodeTimeoutRaw string `yaml:"authorization_code_timeout"`
 	} `yaml:"config"`
+}
+
+func (o OauthPlugin) DatabaseInstance() string {
+	return o.Config.Database
+}
+
+func (o OauthPlugin) AccessTokenTimeout() (time.Duration, error) {
+	return time.ParseDuration(o.Config.AccessTokenTimeoutRaw)
+}
+
+func (o OauthPlugin) AuthorizationCodeTimeout() (time.Duration, error) {
+	return time.ParseDuration(o.Config.AuthorizationCodeTimeoutRaw)
 }
