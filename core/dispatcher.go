73e82450e53fdf458a3c2f52804c8f759236f2ef
insomnius <aliana.arib@gmail.com>
2020-06-10 01:13:10 +0700

    Remove unused interfaces

--- core/dispatcher.go
+++ core/dispatcher.go
@@ -1,26 +1,6 @@
 package core
 
-type OauthDispatcher interface {
-	Application() OauthApplicationDispatcher
-	Authorization() AuthorizationDispatcher
-}
-
-type OauthApplicationDispatcher interface {
-	List(applicationManager ApplicationManager) Controller
-	One(applicationManager ApplicationManager) Controller
-	Create(applicationManager ApplicationManager) Controller
-}
-
-type AuthorizationDispatcher interface {
-	Grant(authorization Authorization) Controller
-	Revoke(authorization Authorization) Controller
-}
-
 type RouteDispatcher interface {
 	Compiler() RouteCompiler
 	Generator() RouteGenerator
-}
-
-type DownstreamPluginDispatcher interface {
-	Oauth(oauthAccessTokenModel OauthAccessTokenModel) DownStreamPlugin
 }
