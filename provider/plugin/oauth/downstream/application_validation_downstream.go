29c674dccd11bf240f6aec192cb5df1f1654e7dd
Muhammad Arief Rahman <aliana.arib@gmail.com>
2020-11-08 07:20:01 +0700

    Add application validation (#30)

---
+++ provider/plugin/oauth/downstream/application_validation_downstream.go
@@ -0,0 +1,75 @@
+package downstream
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	coreEntity "github.com/codefluence-x/altair/entity"
+	"github.com/codefluence-x/altair/provider/plugin/oauth/entity"
+	"github.com/codefluence-x/altair/provider/plugin/oauth/interfaces"
+	"github.com/gin-gonic/gin"
+)
+
+// ApplicationValidation implement downstream plugin interface
+type ApplicationValidation struct {
+	oauthApplicationModel interfaces.OauthApplicationModel
+}
+
+// NewApplicationValidation create new downstream plugin to check the validity of application uid and application secret given by the client
+func NewApplicationValidation(oauthApplicationModel interfaces.OauthApplicationModel) *ApplicationValidation {
+	return &ApplicationValidation{oauthApplicationModel: oauthApplicationModel}
+}
+
+// Name of downstream plugin
+func (o *ApplicationValidation) Name() string {
+	return "application-validation-plugin"
+}
+
+// Intervene current request to check application_uid and application_secret
+func (o *ApplicationValidation) Intervene(c *gin.Context, proxyReq *http.Request, r coreEntity.RouterPath) error {
+	if r.Auth != "oauth_application" {
+		return nil
+	}
+
+	// TODO check header first for CLIENT_UID and CLIENT_SECRET then check the body. If the header exists then continue with header, if not continue with body.
+	// For now we just make it like this.
+	if proxyReq.Body == nil {
+		c.AbortWithStatus(http.StatusUnprocessableEntity)
+		return ErrInvalidRequest
+	}
+
+	bufferString := bytes.NewBuffer(nil)
+	_, err := io.CopyBuffer(bufferString, proxyReq.Body, nil)
+	if err != nil {
+		c.AbortWithStatus(http.StatusServiceUnavailable)
+		return ErrUnavailable
+	}
+
+	applicationJSON := entity.OauthApplicationJSON{}
+	err = json.Unmarshal(bufferString.Bytes(), &applicationJSON)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return ErrBadRequest
+	}
+
+	if applicationJSON.ClientUID == nil || applicationJSON.ClientSecret == nil {
+		c.AbortWithStatus(http.StatusUnprocessableEntity)
+		return ErrInvalidRequest
+	}
+
+	_, err = o.oauthApplicationModel.OneByUIDandSecret(c, *applicationJSON.ClientUID, *applicationJSON.ClientSecret)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return ErrApplicationNotExists
+		}
+
+		c.AbortWithStatus(http.StatusServiceUnavailable)
+		return ErrUnavailable
+	}
+
+	return nil
+}
