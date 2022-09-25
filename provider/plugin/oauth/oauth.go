16beac7c71621111ebd9a5d08c2c06fead1d2ed6
Insomnius <aliana.arib@gmail.com>
2022-09-25 07:31:58 +0700

    Remove already removed functionality

--- provider/plugin/oauth/oauth.go
+++ provider/plugin/oauth/oauth.go
@@ -1,16 +1,10 @@
 package oauth
 
 import (
-	"time"
-
 	"github.com/kodefluence/altair/core"
-	"github.com/kodefluence/altair/provider/plugin/oauth/controller"
 	"github.com/kodefluence/altair/provider/plugin/oauth/downstream"
 	"github.com/kodefluence/altair/provider/plugin/oauth/entity"
-	"github.com/kodefluence/altair/provider/plugin/oauth/formatter"
 	"github.com/kodefluence/altair/provider/plugin/oauth/model"
-	"github.com/kodefluence/altair/provider/plugin/oauth/service"
-	"github.com/kodefluence/altair/provider/plugin/oauth/validator"
 )
 
 // Provide create new oauth plugin provider
@@ -30,19 +24,6 @@ func Provide(appBearer core.AppBearer, dbBearer core.DatabaseBearer, pluginBeare
 		return err
 	}
 
-	var accessTokenTimeout time.Duration
-	var authorizationCodeTimeout time.Duration
-
-	accessTokenTimeout, err = oauthPluginConfig.AccessTokenTimeout()
-	if err != nil {
-		return err
-	}
-
-	authorizationCodeTimeout, err = oauthPluginConfig.AuthorizationCodeTimeout()
-	if err != nil {
-		return err
-	}
-
 	var refreshTokenConfig entity.RefreshTokenConfig
 	refreshTokenConfig.Active = oauthPluginConfig.Config.RefreshToken.Active
 	if refreshTokenConfig.Active {
@@ -54,29 +35,10 @@ func Provide(appBearer core.AppBearer, dbBearer core.DatabaseBearer, pluginBeare
 	}
 
 	// Model
-	oauthApplicationModel := model.NewOauthApplication()
 	oauthAccessTokenModel := model.NewOauthAccessToken()
-	oauthAccessGrantModel := model.NewOauthAccessGrant()
-	oauthRefreshTokenModel := model.NewOauthRefreshToken()
-
-	// // Formatter
-	oauthModelFormatter := formatter.NewModel(accessTokenTimeout, authorizationCodeTimeout, refreshTokenConfig.Timeout)
-	oauthFormatter := formatter.Oauth()
-
-	// Validator
-	oauthValidator := validator.NewOauth(refreshTokenConfig.Active)
-
-	// Service
-	authorization := service.NewAuthorization(oauthApplicationModel, oauthAccessTokenModel, oauthAccessGrantModel, oauthRefreshTokenModel, oauthModelFormatter, oauthValidator, oauthFormatter, refreshTokenConfig.Active, db)
 
 	// DownStreamPlugin
 	oauthDownStream := downstream.NewOauth(oauthAccessTokenModel, db)
-
-	// Controller of /oauth/authorizations
-	authorizationControllerDispatcher := controller.NewAuthorization()
-	appBearer.InjectController(authorizationControllerDispatcher.Grant(authorization))
-	appBearer.InjectController(authorizationControllerDispatcher.Revoke(authorization))
-	appBearer.InjectController(authorizationControllerDispatcher.Token(authorization))
 
 	appBearer.InjectDownStreamPlugin(oauthDownStream)
 
