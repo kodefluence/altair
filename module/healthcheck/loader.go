e400793effcf89cfcdaab217c2301af328e36b8c
Insomnius <aliana.arib@gmail.com>
2022-09-30 19:31:06 +0700

    Create healthcheck controller

--- module/healthcheck/loader.go
+++ module/healthcheck/loader.go
@@ -1,1 +1,10 @@
 package healthcheck
+
+import (
+	"github.com/kodefluence/altair/module"
+	"github.com/kodefluence/altair/module/healthcheck/controller/http"
+)
+
+func Load(app module.App) {
+	app.Controller().InjectHTTP(http.NewHealthController())
+}
