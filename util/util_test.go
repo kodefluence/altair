f4a5dbb3b9ca245a586a72a28b06ec4d360c32ac
Insomnius <aliana.arib@gmail.com>
2022-12-16 22:52:08 +0700

    test: add unit test for util.ReadFileContent method

--- util/util_test.go
+++ util/util_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/kodefluence/altair/testhelper"
 	"github.com/kodefluence/altair/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -24,4 +25,20 @@ func TestUtil(t *testing.T) {
 	now := time.Now()
 	assert.Equal(t, now, util.PointerToTime(util.TimeToPointer(now)))
 	assert.Equal(t, "xx", util.PointerToString(util.StringToPointer("xx")))
+}
+
+func TestUtil_ReadFileContent(t *testing.T) {
+
+	t.Run("Given valid file name, then it does return file content as a string", func(t *testing.T) {
+		testhelper.GenerateTempTestFiles("./test_file/", "test", "valid_file.txt", 0644)
+		content, err := util.ReadFileContent("./test_file/valid_file.txt")
+		assert.Nil(t, err)
+		assert.Equal(t, []byte("test"), content)
+		testhelper.RemoveTempTestFiles("./test_file/valid_file.txt")
+	})
+
+	t.Run("Given invalid file name, then it does return error", func(t *testing.T) {
+		_, err := util.ReadFileContent("./test_file/invalid_file.txt")
+		assert.NotNil(t, err)
+	})
 }
